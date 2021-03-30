c7132f2fce8e0905af05dd277b87398f9beaed4c
vahdian <germanpalero@gmail.com>
2021-03-30 22:08:37 +0200

    register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { API } from "../../shared/consts/api.const";
 
 export default function Register() {
   const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+    API.post("/register", data).then((res) => {
+      console.log("OK");
+    });
+  };
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
